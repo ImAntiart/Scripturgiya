bae2a49c66eeceec30348611879e336f6b13ce58
Antiart <r2d2rom@mail.ru>
2025-02-05 19:53:23 +0500

    Создан футер

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 // src/App.tsx
 import React from 'react';
-import CardList from './components/CardList';
-import cardData from './data/cardData';
 import './App.css'; // Импортируем стили
+import Footer from './components/3.footer/footer';
 
 const App: React.FC = () => {
   return (
     <div className="app-container">
-      <CardList cardData={cardData} />
+
+      <Footer/>
     </div>
   );
 };
---
+++ src/components/3.footer/Footer.tsx
@@ -0,0 +1,10 @@
+
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>© Разработчик Antiart, aka Ромашев Алексей Дмитриевич. Все права обязательно будут защищены, но не сейчас</p>
+    </footer>
+  );
+}
